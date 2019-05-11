fed5b5f5aa487471f84cb59170b501081004b031
Mike McMahon <mmm219@mcc.northwestern.edu>
2019-05-10 21:56:26 -0500

    reset game added but not final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,6 +7,8 @@ var wins = 0;
 console.log(wins);
 var losses = 0;
 console.log(losses);
+var guessCount = 9;
+console.log(guessCount);
 
 // creates java script variables that link to the html
 var winsText = document.getElementById("wins-text");
@@ -14,6 +16,13 @@ console.log(winsText);
 var lossesText = document.getElementById("losses-text");
 var remainingText = document.getElementById("remaining-text");
 var guessText = document.getElementById("guess-text");
+
+// resets the game
+resetGame ()= {
+
+
+    
+}
 
 document.onkeyup = function (event) {
     // record user keystroke
@@ -26,16 +35,26 @@ document.onkeyup = function (event) {
     // if-else clauses that add to wins and losses tallies
     
     if (userGuess === computerGuess) {
-        winsText++;
+        wins++;
+        alert ("Correct!");
     } else {
-        lossesText++;
+        // alert(guessCount-1 + " Wrong! You suck at this!");
+        guessCount = guessCount-1;
+        // guessText = guessText + userGuess + ", ";
     }
 
-    guessText.textContent = "Your guesses so far: " + userGuess;
+    if (guessCount === 0) {                
+        losses++;
+        resetGame();
+    }
 
     
+    // adding text to the div and reporting letters, wins, losses, etc.
 
-
+    remainingText.textContent = "Guesses remaining: " + guessCount;
+    winsText.textContent = "Wins: " + wins;
+    lossesText.textContent = "Losses: " + losses;
+    guessText.textContent = "Your guesses so far: " + userGuess;
 
 
     };
